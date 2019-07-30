e3c75e824d0fa7bd8db6c4d95e8ddff67169185b
unknown <arung.agamani@gmail.com>
2019-07-30 18:11:33 +0700

    [Blog] Fixed parameters that breaks the URL

--- controller/blogRoute.js
+++ controller/blogRoute.js
@@ -1,7 +1,8 @@
 const Post = require('../models/BlogPost');
+const url = require('url');
 
 module.exports = async (req, res) => {
-    const origUrl = req.originalUrl;
+    const origUrl = url.parse(req.originalUrl).pathname;
     const postLink = origUrl.slice(6);
     const posts = await Post.find({ link : postLink });
     let logged = false;
